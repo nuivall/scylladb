f9b4879248acb4bcdb0bc4a2b3929af78cfb8a81
Marcin Maliszkiewicz <marcinmal@scylladb.com>
2023-12-08 17:12:33 +0100

    db: add system_auth_v2 keyspace
    New keyspace is added similarly as system_schema keyspace,
    it's being registred via system_keyspace::make which calls
    all_tables to build its schema.
    
    Keyspace and tables are added unconditionally to simplify the
    implementation but nothing will be using it unless feature flag
    is turned on.
    
    Dummy table 'roles' is added as keyspaces are being currently
    registered by walking through their tables. Full table schemas
    will be added in subsequent commits.
    
    Change can be observed via cqlsh:
    
    cassandra@cqlsh> describe keyspaces;
    
    system_auth_v2  system_schema       system         system_distributed_everywhere
    system_auth     system_distributed  system_traces
    
    cassandra@cqlsh> describe keyspace system_auth_v2;
    
    CREATE KEYSPACE system_auth_v2 WITH replication = {'class': 'LocalStrategy'}  AND durable_writes = true;
    
    CREATE TABLE system_auth_v2.roles (
        role text PRIMARY KEY
    ) WITH bloom_filter_fp_chance = 0.01
        AND caching = {'keys': 'ALL', 'rows_per_partition': 'ALL'}
        AND comment = 'comment'
        AND compaction = {'class': 'SizeTieredCompactionStrategy'}
        AND compression = {'sstable_compression': 'org.apache.cassandra.io.compress.LZ4Compressor'}
        AND crc_check_chance = 1.0
        AND dclocal_read_repair_chance = 0.0
        AND default_time_to_live = 0
        AND gc_grace_seconds = 604800
        AND max_index_interval = 2048
        AND memtable_flush_period_in_ms = 0
        AND min_index_interval = 128
        AND read_repair_chance = 0.0
        AND speculative_retry = '99.0PERCENTILE';

---
+++ db/system_auth_keyspace.hh
@@ -0,0 +1,26 @@
+/*
+ * Modified by ScyllaDB
+ * Copyright (C) 2024-present ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: (AGPL-3.0-or-later and Apache-2.0)
+ */
+
+#pragma once
+
+#include "db/config.hh"
+#include "schema/schema_fwd.hh"
+#include <vector>
+
+namespace db {
+
+namespace system_auth_keyspace {
+    static constexpr auto NAME = "system_auth_v2";
+    // tables
+    static constexpr auto ROLES = "roles";
+
+    std::vector<schema_ptr> all_tables(const db::config& cfg);
+}; // namespace system_auth_keyspace
+
+} // namespace db
