77ff14df34d3f11bd91ec0a1182725edbee6373d
Marcin Maliszkiewicz <marcinmal@scylladb.com>
2025-02-20 15:04:38 +0100

    generic_server: add semaphore for limiting new connections concurrency
    It will be used in following commits.

--- generic_server.hh
+++ generic_server.hh
@@ -12,6 +12,7 @@
 #include "utils/log.hh"
 
 #include "seastarx.hh"
+#include "utils/updateable_value.hh"
 
 #include <list>
 
@@ -41,6 +42,12 @@ public:
     using connection_process_loop = noncopyable_function<future<> ()>;
     using execute_under_tenant_type = noncopyable_function<future<> (connection_process_loop)>;
     bool _tenant_switch = false;
+    struct cpu_concurrency_t {
+        lw_shared_ptr<named_semaphore> semaphore;
+        semaphore_units<named_semaphore_exception_factory> units;
+        bool stopped;
+    };
+    cpu_concurrency_t _conns_cpu_concurrency;
     execute_under_tenant_type _execute_under_current_tenant = no_tenant();
 protected:
     server& _server;
@@ -54,7 +61,7 @@ protected:
 private:
     future<> process_until_tenant_switch();
 public:
-    connection(server& server, connected_socket&& fd);
+    connection(server& server, connected_socket&& fd, lw_shared_ptr<named_semaphore> sem, semaphore_units<named_semaphore_exception_factory> initial_sem_units);
     virtual ~connection();
 
     virtual future<> process();
@@ -110,7 +117,9 @@ protected:
     std::list<gentle_iterator> _gentle_iterators;
     std::vector<server_socket> _listeners;
     shared_ptr<seastar::tls::server_credentials> _credentials;
-
+private:
+    utils::updateable_value<uint32_t> _conns_cpu_concurrency;
+    lw_shared_ptr<named_semaphore> _conns_cpu_concurrency_semaphore;
 public:
     server(const sstring& server_name, logging::logger& logger, const db::config& db_cfg);
 
@@ -135,7 +144,7 @@ public:
     future<> do_accepts(int which, bool keepalive, socket_address server_addr);
 
 protected:
-    virtual seastar::shared_ptr<connection> make_connection(socket_address server_addr, connected_socket&& fd, socket_address addr) = 0;
+    virtual seastar::shared_ptr<connection> make_connection(socket_address server_addr, connected_socket&& fd, socket_address addr, lw_shared_ptr<named_semaphore> sem, semaphore_units<named_semaphore_exception_factory> initial_sem_units) = 0;
 
     virtual future<> advertise_new_connection(shared_ptr<connection> conn);
 
