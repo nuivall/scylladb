288a5193bef35238aa9914fa8ac58a36930ffa9b
Marcin Maliszkiewicz <marcinmal@scylladb.com>
2026-09-07 16:53:48 +0200

    cql3: modification_statement: make do_execute protected virtual
    The execution step is the only part a strongly consistent modification
    needs to replace.

--- cql3/statements/modification_statement.hh
+++ cql3/statements/modification_statement.hh
@@ -226,9 +226,13 @@ public:
      */
     bool applies_to(const selection::selection* selection, const update_parameters::prefetch_data::row* row, const query_options& options) const;
 
-private:
-    future<::shared_ptr<cql_transport::messages::result_message>>
+protected:
+    // Performs the write itself. Overridable so that a subclass can commit the
+    // mutation through a different backend while inheriting the rest of the
+    // statement.
+    virtual future<::shared_ptr<cql_transport::messages::result_message>>
     do_execute(query_processor& qp, service::query_state& qs, const query_options& options) const;
+private:
     friend class modification_statement_executor;
 public:
     // True if the statement has IF conditions. Pre-computed during prepare.
