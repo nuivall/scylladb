ffbe15918f0e8a8a3fae85f9ac5f779c131f81ca
Marcin Maliszkiewicz <marcinmal@scylladb.com>
2026-09-21 01:28:45 +0200

    test/cluster/dtest: port replace-address and top-partitions tests

--- test/cluster/dtest/toppartitions_test.py
+++ test/cluster/dtest/toppartitions_test.py
@@ -1,3 +1,9 @@
+#
+# Copyright (C) 2025-present ScyllaDB
+#
+# SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.1
+#
+
 import logging
 import math
 import re
@@ -29,9 +35,7 @@ from tools.toppartitions import (
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.dtest_full
 @pytest.mark.single_node
-@pytest.mark.next_gating
 class TestTopPartitions(Tester):
     """Class to test new functioanality of nodetool command toppartitions
 
