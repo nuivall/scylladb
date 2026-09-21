c0408edd4a9efcce42229062beb98bd18a31bc9f
Marcin Maliszkiewicz <marcinmal@scylladb.com>
2026-09-21 21:15:19 +0200

    test/dtest: port CQL system-clients tests

--- test/cluster/dtest/cql_system_clients_test.py
+++ test/cluster/dtest/cql_system_clients_test.py
@@ -3,6 +3,7 @@
 #
 # SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.1
 #
+
 import hashlib
 import logging
 import os
@@ -139,8 +140,6 @@ class CQLSession:
         return int.from_bytes(hashlib.md5(str(self).encode("utf8")).digest(), "little")
 
 
-@pytest.mark.dtest_full
-@pytest.mark.next_gating
 class TestSystemClients(Tester):
     _test_users = [
         {"user": "user1", "password": "password1"},
@@ -223,7 +222,7 @@ class TestSystemClients(Tester):
         cluster = self.cluster
         if ssl_enabled:
             ip_addresses = [f"{cluster.get_ipprefix()}{i}" for i in range(1, nodes + 1)]
-            generate_ssl_stores(self.test_path, ip_addresses=ip_addresses)
+            generate_ssl_stores(self.cluster.get_path(), ip_addresses=ip_addresses)
         # C* versions before 3.0 (CASSANDRA-10559) do not know about
         # 'client_encryption_options.optional' - so we must not add that parameter
         # Note: does of course not work with scylla, we dont support "optional" (3.x feature)
@@ -234,18 +233,18 @@ class TestSystemClients(Tester):
             ssl_options["optional"] = ssl_optional
 
         if common.isScylla(cluster.get_install_dir()):
-            ssl_options.update({"certificate": os.path.join(self.test_path, "ccm_node.pem"), "keyfile": os.path.join(self.test_path, "ccm_node.key")})
+            ssl_options.update({"certificate": os.path.join(self.cluster.get_path(), "ccm_node.pem"), "keyfile": os.path.join(self.cluster.get_path(), "ccm_node.key")})
             if require_ssl_auth:
-                ssl_options.update({"truststore": os.path.join(self.test_path, "ccm_node.cer"), "require_client_auth": True})
+                ssl_options.update({"truststore": os.path.join(self.cluster.get_path(), "ccm_node.cer"), "require_client_auth": True})
         else:
             ssl_options.update(
                 {
-                    "keystore": os.path.join(self.test_path, "keystore.jks"),
+                    "keystore": os.path.join(self.cluster.get_path(), "keystore.jks"),
                     "keystore_password": "cassandra",
                 }
             )
             if require_ssl_auth:
-                ssl_options.update({"truststore": os.path.join(self.test_path, "truststore.jks"), "truststore_password": "cassandra", "require_client_auth": True})
+                ssl_options.update({"truststore": os.path.join(self.cluster.get_path(), "truststore.jks"), "truststore_password": "cassandra", "require_client_auth": True})
         cluster.set_configuration_options(
             {
                 "client_encryption_options": ssl_options,
@@ -300,7 +299,7 @@ class TestSystemClients(Tester):
 
         # Success SSL connection test
         ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-        ssl_context.load_verify_locations(cafile=os.path.join(self.test_path, "ccm_node.cer"))
+        ssl_context.load_verify_locations(cafile=os.path.join(self.cluster.get_path(), "ccm_node.cer"))
 
         with self.node_session(1, **self._test_users[0], port=9142, session_store=session_store, ssl_context=ssl_context):
             session_store.expect_system_clients(tester=self)
@@ -390,9 +389,9 @@ class TestSystemClients(Tester):
         session_store = SessionStore()
 
         ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-        ssl_context.load_cert_chain(certfile=os.path.join(self.test_path, "ccm_node.pem"), keyfile=os.path.join(self.test_path, "ccm_node.key"))
+        ssl_context.load_cert_chain(certfile=os.path.join(self.cluster.get_path(), "ccm_node.pem"), keyfile=os.path.join(self.cluster.get_path(), "ccm_node.key"))
         ssl_context.verify_mode = ssl.CERT_REQUIRED
-        ssl_context.load_verify_locations(cafile=os.path.join(self.test_path, "ccm_node.cer"))
+        ssl_context.load_verify_locations(cafile=os.path.join(self.cluster.get_path(), "ccm_node.cer"))
 
         # Successful SSL authentication test
         with self.node_session(
@@ -406,7 +405,7 @@ class TestSystemClients(Tester):
         session_store.expect_system_clients(tester=self)
 
         ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-        ssl_context.load_verify_locations(cafile=os.path.join(self.test_path, "ccm_node.cer"))
+        ssl_context.load_verify_locations(cafile=os.path.join(self.cluster.get_path(), "ccm_node.cer"))
 
         # Failed SSL authentication test
         with self.patient_cql_connection(self.cluster.nodelist()[0], user="cassandra", password="cassandra") as session:
@@ -431,7 +430,7 @@ class TestSystemClients(Tester):
         if ssl_optional:
             port = 9142
             ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-            ssl_context.load_verify_locations(cafile=os.path.join(self.test_path, "ccm_node.cer"))
+            ssl_context.load_verify_locations(cafile=os.path.join(self.cluster.get_path(), "ccm_node.cer"))
         else:
             port = 9042
             ssl_context = None
@@ -461,13 +460,11 @@ class TestSystemClients(Tester):
         fields = ["address", "port", "client_type", "connection_stage", "protocol_version", "shard_id", "username", "driver_name", "driver_version"]
         self._system_client_content(fields)
 
-    @pytest.mark.require("#9216")
     @pytest.mark.single_node
     def test_system_client_hostname(self):
         fields = ["hostname"]
         self._system_client_content(fields)
 
-    @pytest.mark.require("#9216")
     @pytest.mark.single_node
     def test_system_client_ssl(self):
         fields = ["ssl_cipher_suite", "ssl_enabled", "ssl_protocol"]
@@ -478,7 +475,6 @@ class TestSystemClients(Tester):
         fields = ["address", "port", "client_type", "connection_stage", "protocol_version", "shard_id", "username", "driver_name", "driver_version"]
         self._system_client_content(fields, ssl_optional=False, ssl_enabled=False)
 
-    @pytest.mark.require("#9216")
     @pytest.mark.single_node
     def test_system_client_hostname_non_ssl(self):
         fields = ["hostname"]
